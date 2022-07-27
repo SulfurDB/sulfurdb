f3f8d6a0c86012dc44bd2771871c616cdf35582c
Joshua Weinstein <jweinstein@splunk.com>
2022-07-27 01:25:25 -0700

    unit tests

--- sulfur_core/core_tests/sulfur_core_data_set_tests.c
+++ sulfur_core/core_tests/sulfur_core_data_set_tests.c
@@ -2,16 +2,16 @@
 #include "sulfur_data_set.h"
 
 
-static void test_sulfur_data_set_init(void)
+static void test_sulfur_dataset_init(void)
 {
-	sulfur_data_set_t set;
+	sulfur_dataset_t set;
 	double values[] = {4.0, 4.0, 5.5, 3.0, 2.2, 2.1};
-	sulfur_data_set_init(&set, values, sizeof(values), 2, 2);
+	sulfur_dataset_init(&set, values, sizeof(values), 2, 2);
 	UNIT_CHECK(set.data != NULL);
 	UNIT_CHECK(set.size = sizeof(values));
 	UNIT_CHECK(set.dimensions = 2);
 	UNIT_CHECK(set.precision = 2);
-	sulfur_data_set_deinit(&set);
+	sulfur_dataset_deinit(&set);
 	UNIT_CHECK(set.data == NULL);
 }
 
@@ -19,6 +19,6 @@ static void test_sulfur_data_set_init(void)
 
 int main(int argc, char const *argv[])
 {
-	test_sulfur_data_set_init();
+	test_sulfur_dataset_init();
 	return _failure_result();
 }
